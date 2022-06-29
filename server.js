1f3a4f157702b915e40c1245220b754cda1c47cd
Stella Sikhila <stellavinmaris@gmail.com>
2022-06-29 19:11:38 +0300

    changed port

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 require("./app/routes/track.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
