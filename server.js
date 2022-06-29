c0803b6a0164207b454af3ec14132ff7dd34c809
Stella Sikhila <stellavinmaris@gmail.com>
2022-06-29 19:31:28 +0300

    added firebase url

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://deezer-web.web.app",
 };
 
 app.use(cors(corsOptions));
